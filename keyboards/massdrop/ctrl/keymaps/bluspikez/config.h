778f959af1edf4c201f75320e65e331698d07fef
Matt Smith <bluspikez@gmail.com>
2021-06-14 15:18:43 -0400

    change default color; assign all F1-F12 as mods

--- keyboards/massdrop/ctrl/keymaps/bluspikez/config.h
+++ keyboards/massdrop/ctrl/keymaps/bluspikez/config.h
@@ -45,7 +45,7 @@
 #define RGB_MATRIX_KEYPRESSES         // reacts to keypresses
 // #define RGB_MATRIX_KEYRELEASES        // reacts to keyreleases (instead of keypresses)
 #define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_ALPHAS_MODS // Sets the default mode, if none has been set
-#define RGB_MATRIX_STARTUP_HUE 9 // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_HUE 29 // Sets the default hue value, if none has been set
 #define RGB_MATRIX_STARTUP_SAT 255 // Sets the default saturation value, if none has been set
 #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
 #define RGB_MATRIX_STARTUP_SPD 85 // Sets the default animation speed, if none has been set
--- keyboards/massdrop/ctrl/keymaps/bluspikez/config_led.c
+++ keyboards/massdrop/ctrl/keymaps/bluspikez/config_led.c
@@ -45,8 +45,8 @@ led_config_t g_led_config = { {
     { 149,   0 }, { 167,   0 }, { 186,   0 }, { 204,   0 }, { 224,   0 }, { 224,  17 }, { 224,  32 }, { 224,  47 }
 }, {
   // KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS
-    1, 4, 4, 4, 4, 1, 1, 1,
-    1, 4, 4, 4, 4, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
   // KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP
     4, 4, 4, 4, 4, 4, 4, 4,
     4, 4, 4, 4, 4, 1, 1, 1,
